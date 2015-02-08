2d9d485aba40edd22a6bf6c595d15c2c85a42dd7
Robert Porter <robert.j.porter10@gmail.com>
2015-02-08 10:02:38 -0600

    corrected spawning logic partially

--- js/spawner.js
+++ js/spawner.js
@@ -3,19 +3,24 @@
 var Spawner = {
 
     init: function () {
-        this.waveInterval = 3000;
+        this.waveInterval = 5000;
         this.lastWaveTime = Date.now();
+		this.spawn();
     },
     update: function (deltaTime) {
+		/*
         var now = Date.now();
         if (now - this.lastWaveTime > this.waveInterval) {
             this.lastWaveTime = now;
             this.spawn();
         }
+		*/
     },
     spawn: function () {
-        for (var i = 0; i < 10; i++) {
-            Game.addGameObject(new Invader(World.WIDTH * 0.5 - 250 + i * 50, 30));
-        }
+		for(var y = 0; y < 5; y++) {
+			for (var x = 0; x < 11; x++) {
+				Game.addGameObject(new Invader(World.WIDTH * 0.5 - 250 + x * 50, y * 50));
+			}
+		}
     }
 };
