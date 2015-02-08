4a4da9cba453928f9c021ca52bc3aff7bc4799e1
Robert Porter <robert.j.porter10@gmail.com>
2015-02-08 10:48:44 -0600

    delete offscreen bullets

--- js/bullet.js
+++ js/bullet.js
@@ -6,3 +6,12 @@ function Bullet(x, y) {
     GameObject.call(this, x, y, 12, 12);
     this.velY = -100;
 }
+
+Bullet.prototype.update = function(deltaTime) { 
+	GameObject.prototype.update.call(this, deltaTime);
+	
+	if(this.x + this.width < 0 || this.x > World.WIDTH ||
+		this.y + this.height < 0 ||	this.y > World.HEIGHT) {
+			this.dead = true;
+	}
+}
