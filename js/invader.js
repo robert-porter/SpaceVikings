73534d3364c6465e186fdf43dfa3cdd661e3442e
Hydrothermal <curradid@yahoo.com>
2015-02-28 16:07:12 -0500

    Re-added music with new sounds

--- js/invader.js
+++ js/invader.js
@@ -1,15 +1,15 @@
-﻿// function AudioBank(file, count) {
-// 	this.bank = [];
-// 	for(var i = 0; i < count; i++) {
-// 		this.bank[i] = new Audio(file);
-// 	}
-// 	this.last = 0;
-// }
+﻿function AudioBank(file, count) {
+	this.bank = [];
+	for(var i = 0; i < count; i++) {
+		this.bank[i] = new Audio(file);
+	}
+	this.last = 0;
+}
 
-// AudioBank.prototype.play = function() {
-// 	this.last = (this.last + 1) % this.bank.length;
-// 	this.bank[this.last].play();
-// }
+AudioBank.prototype.play = function() {
+	this.last = (this.last + 1) % this.bank.length;
+	this.bank[this.last].play();
+}
 
 var InvadersGroup = {
 	LEFT: 0,
@@ -34,11 +34,13 @@ var InvadersGroup = {
 	posX: 40,
 	posY: 0,
 	
-	// audio: [new AudioBank("audio/snd1.mp3", 4),
-	// 		new AudioBank("audio/snd2.mp3", 4),
-	// 		new AudioBank("audio/snd3.mp3", 4),
-	// 		new AudioBank("audio/snd4.mp3", 4)	],
-	// currentAudio: 0,
+	audio: [
+		new AudioBank("audio/move_1.mp3", 4),
+		new AudioBank("audio/move_2.mp3", 4),
+		new AudioBank("audio/move_3.mp3", 4),
+		new AudioBank("audio/move_4.mp3", 4)
+	],
+	currentAudio: -1,
 	invaders: [],
 
 	init: function() {
@@ -165,8 +167,8 @@ var InvadersGroup = {
 		return true;
 	},
 	move: function(deltaTime) {
-		// this.currentAudio = (this.currentAudio + 1) % this.audio.length;
-		// this.audio[this.currentAudio].play();
+		this.currentAudio = (this.currentAudio + 1) % this.audio.length;
+		this.audio[this.currentAudio].play();
 		
 		var leftIndex = this.getLeftBoundaryXIndex();
 		var rightIndex = this.getRightBoundaryXIndex();
